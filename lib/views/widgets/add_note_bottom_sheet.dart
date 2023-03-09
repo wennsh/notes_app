5e01e13070880588b49ccb482b06215f3ff66121
wennsh <kingmahmoud3377@gmail.com>
2023-03-09 21:46:34 +0200

    show model bottom sheet

---
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AddNoteButtomSheet extends StatelessWidget {
+  const AddNoteButtomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
